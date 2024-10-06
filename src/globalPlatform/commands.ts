5dc17f860f60de5c85ba17b5e418308cde02d6f4
a.vitsiuk <a.vitsiuk@affidaty.io>
2024-10-06 17:30:11 +0200

    GP extAuth command

--- src/globalPlatform/commands.ts
+++ src/globalPlatform/commands.ts
@@ -51,78 +51,28 @@ export function initUpdate(
 }
 
 /**
- * External authenticate
- * @param secLvl - (Default:`0`) Defines the level of security for all secure messaging commands following this EXTERNAL AUTHENTICATE command (it does not apply to this command) and within this Secure Channel
- * Possible `secLvl` values:
- * `0` - No secure messaging expected
- * `1` - C-MAC
- * `3` - C-DECRYPTION and C-MAC
+ * The EXTERNAL AUTHENTICATE command is used by the card, during explicit initiation of a Secure Channel, to authenticate the host and to determine the level of security required for all subsequent commands.
+ * @param secLvl - (Default:`0`) Defines the level of security for all secure messaging commands following this EXTERNAL AUTHENTICATE command (it does not apply to this command) and within this Secure Channel. Valid `secLvl` values:
+ * - `0x00` - No secure messaging expected
+ * - `0x01` - C-MAC
+ * - `0x03` - C-DECRYPTION, C-MAC
+ * - `0x10` - R-MAC
+ * - `0x11` - R-MAC, C-MAC
+ * - `0x13` - R-MAC, C-DECRYPTION, C-MAC
  */
-export function extAuth(hostCryptogram: TBinData, secLvl: 0 | 1 | 3 = 0) {
+export function extAuth(hostCryptogram: TBinData, secLvl: 0 | 1 | 3 | 16 | 17 | 19 = 0) {
     let cmd = new CommandApdu()
         .setProprietary()
-        .setType(4)
         .setSecMgsType(1)
         .setIns(isoIns.EXT_MUT_AUTH)
         .setP1(secLvl)
-        .setP2(0x00)
-        .setData(hostCryptogram);
-    return cmd;
-}
+        .setP2(0x00);
 
-/**
- * Internal authenticate
- * @param key - ephemeral OCE key agreement public key
- * @param secLvl - (Default:`0x34`) Defines the level of security for all secure messaging commands following this INTERNAL_AUTHENTICATE command (it does not apply to this command) and within this Secure Channel
- * Possible `secLvl` values:
- * `0x34` - C-MAC and R-MAC only
- * `0x3C` - C-MAC, C-DECRYPTION, R-MAC, R-ENCRYPTION
- * @param includeId - (Default: `false`) If true, a passed id can be included
- * @param id - id to include if `includeId` parameter has been set to `true`
- */
-export function intAuth(
-    key: TBinData,
-    secLvl: 0x34 | 0x3c = 0x34,
-    includeId: boolean = false,
-    id: TBinData = new Uint8Array(0),
-) {
-    let _key: Uint8Array;
     try {
-        _key = importBinData(key);
+        cmd.setData(hostCryptogram);
     } catch (error: any) {
-        throw new Error(`Key error: ${error.message}`);
+        throw new Error(`extAuth command error: ${error}`)
     }
 
-    let berObjInfo: IBerObjInfo = {
-        tag: Tag.root,
-        value: [
-            {
-                tag: 'A6',
-                value: [
-                    { tag: '90', value: [0x11, includeId ? 0x04 : 0x00] },
-                    { tag: '95', value: [secLvl] },
-                    { tag: '80', value: [0x88] },
-                    { tag: '81', value: [Math.floor(_key.byteLength / 2)] },
-                ],
-            },
-            { tag: '5F49', value: _key },
-        ],
-    };
-
-    if (includeId) {
-        ((berObjInfo.value as IBerObjInfo[])[0].value as IBerObjInfo[]).push({
-            tag: '84',
-            value: id,
-        });
-    }
-
-    let cmd = new CommandApdu()
-        .setProprietary()
-        .setType(4)
-        .setSecMgsType(0)
-        .setIns(isoIns.INT_AUTH)
-        .setP1(0x00) // key version
-        .setP2(0x00) // key identifier
-        .setData(BerObject.create(berObjInfo).serialize());
     return cmd;
 }
