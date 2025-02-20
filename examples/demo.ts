b460bf77871e30185f00dd59d6bc959f1feefdf4
avitsiuk <aliaksei.vitsiuk@gmail.com>
2025-02-20 15:01:24 +0100

    removed code committed by mistake

--- examples/demo.ts
+++ examples/demo.ts
@@ -1,13 +1,18 @@
 import {
+    Logger,
     PcscDevicesManager,
     Device,
     Card,
     CommandApdu,
     Utils,
     BER,
-    Iso7816,
-    GP
+    Iso7816Commands,
+    GPCommands,
+    GPUtils,
+    ResponseApdu,
 } from '../src/index';
+
+Logger.setLogLevel(Logger.ELogLevel.INFO); // NONE(0), FATAL(1), ERROR(2), WARN(3), INFO(4), DEBUG(5), TRACE(6)
 
 const devices: {[key: string]: {device: Device, card: Card | null}} = {};
 
@@ -61,31 +66,32 @@ pcscDM.on('device-activated', (event => {
         devices[device.name].card = event.card;
         printDeviceList();
 
-        console.log(Utils.decodeAtr(event.card.atr));
+        const card = event.card;
+
+        // logging card communications
+        // event.card.on('command-issued', (event) => {
+        //     console.log(`[${device.name}][CMD]<< [${event.command}]`)
+        // })
+        // event.card.on('response-received', (event) => {
+        //     console.log(`[${device.name}][RSP]>> [${Utils.hexEncode([...event.response.data])}][${Utils.hexEncode([...event.response.status])}](${event.response.meaning})`)
+        // })
+
+        console.log(Utils.decodeAtr(card.atr));
 
         console.log('Selecting default applet...');
         console.log();
-
-        event.card.on('command-issued', (event) => {
-            console.log(`[${device.name}][CMD]<< [${event.command}]`)
-        })
-
-        event.card.on('response-received', (event) => {
-            console.log(`[${device.name}][RSP]>> [${Utils.hexEncode([...event.response.data])}][${Utils.hexEncode([...event.response.status])}](${event.response.meaning})`)
-        })
 
         // Uncomment this to disable autoGetResponse feature
         // In that case GET_RESPONSE commands must be sent manually
         // event.card.autoGetResponse = false;
 
-        // event.card.issueCommand(Iso7816Commands.select('429999990000'))
-        event.card.issueCommand(Iso7816.commands.select())
+        event.card.issueCommand(Iso7816Commands.select())
             .then((selectResponse) => {
                 console.log();
 
                 let berObj: BER.BerObject | undefined;
                 try {
-                    berObj = BER.BerObject.parse(selectResponse.data)
+                    berObj = BER.BerObject.parse(selectResponse.data);
                 } catch (error) {
                     // decode error, probably not BER
                 }
@@ -93,47 +99,22 @@ pcscDM.on('device-activated', (event => {
                 if (berObj && selectResponse.data.byteLength > 0) {
                     console.log('Decoded card response BER:');
                     berObj.print();
-                    // custom print function
-                    // BER.BerObject.parse(selectResponse.data).print((obj, lvl, line) => {
+                    // // custom print function
+                    // berObj.print((line, lvl, obj) => {
                     //     console.log(`[${obj.isPrimitive() ? 'P': obj.isRoot() ? 'R' : 'C'}][${lvl}]${line}`);
                     // });
                 } else {
                     console.log('Card response:');
                     console.log(`${selectResponse.toString()} (${selectResponse.meaning})`);
                 }
-
-
-                /*
-                    Uncomment following lines to try establish a new SCP02 session
-                    Note that default GP test keys are used (gpDefaultStaticKeys).
-                    If card keys have been personlized, you must provide them
-                    in order to successfully initiate a new secure session.
-                */
-
+            })
+            .then(() => {
+                // get info from a GlobalPlatform card
+                return GPUtils.getInfo(card);
+            }).then((cardInfo) => {
                 console.log();
-                console.log(`Initiating SCP02 session`);
-
-                const scp = new GP.SCP02(event.card).setStaticKeys(GP.values.defaultStaticKeys).setSecurityLevel(3);
-
-                scp.initAndAuth()
-                    .then(()=>{
-                        console.log('Secure session established');
-                        console.log();
-                        const getCardDataCmd = new CommandApdu('80CA0066'); // getting card info. This command wil be transformed before submission
-                        event.card.issueCommand(getCardDataCmd)
-                            .then((cardDataResponse) => {
-                                console.log(cardDataResponse);
-                            })
-                            .catch((error) => {
-                                console.log('Error getting card info');
-                                console.log(error);
-                            })
-                    })
-                    .catch((error) => {
-                        console.error('SCP02 error:');
-                        console.error(error);
-                        console.log();
-                    })
+                console.log('Card info:')
+                console.log(cardInfo);
             })
             .catch((e) => {
                 console.log();
